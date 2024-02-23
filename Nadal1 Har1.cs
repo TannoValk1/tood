5f791ad55867ff005da44d31148d6f1a512b28ff
TannoValk1 <158517011+TannoValk1@users.noreply.github.com>
2024-02-23 11:26:38 +0200

    Add files via upload

---
+++ Nadal1 Har1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rand = new Random();
+
+        int num1 = rand.Next(1, 11);
+
+        Console.WriteLine("Arva mis numbrile ma mõtlen vahemikus 1-10?:");
+        int guess = Convert.ToInt32(Console.ReadLine());
+
+        if (guess == num1)
+        {
+            Console.WriteLine("Sa arvasid mu numbri ära!");
+        }
+        else
+        {
+            Console.WriteLine($"Sa ei arvanud mu numbrit ära. Õige number oli {num1}.");
+        }
+    }
+}
