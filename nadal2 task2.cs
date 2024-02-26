30bb5aceb574cafb75a682276c2c73f78f30a2da
TannoValk1 <158517011+TannoValk1@users.noreply.github.com>
2024-02-26 15:38:25 +0200

    Add files via upload

---
+++ nadal2 task2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var stringsList = new List<string>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write($"Please enter 3 strings: {i + 1}: ");
+            stringsList.Add(Console.ReadLine());
+        }
+
+        stringsList.Sort();
+
+        Console.WriteLine("\nResult in sorted list are::");
+        foreach (var str in stringsList)
+            Console.WriteLine(str);
+    }
+}
