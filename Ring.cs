70f18a45b8a38e1f7bed94a01050c0053579376e
TannoValk1 <158517011+TannoValk1@users.noreply.github.com>
2024-03-08 14:00:34 +0200

    Add files via upload

---
+++ Ring.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Ring {
+    private double _raadius;
+    private double _läbimõõt;
+
+    public Ring() {
+        _raadius = 0;
+        _läbimõõt = 0;
+    }
+
+    public Ring(double raadius) {
+        _raadius = raadius;
+        _läbimõõt = raadius * 2;
+    }
+
+    public void PrindiInfo() {
+        if (_raadius == 0 || _läbimõõt == 0) {
+            Console.WriteLine("ring moodud pole sisestatud!");
+        } else {
+            Console.WriteLine($"ringi raadius: {_raadius}läbimõõt:{_läbimõõt}");
+        }
+    }
+
+    public void MääraRaadius(double raadius) {
+        _raadius = raadius;
+        _läbimõõt = raadius * 2;
+        PrindiInfo();
+    }
+
+    public void MääraLäbimõõt(double läbimõõt) {
+        _raadius = läbimõõt / 2;
+        _läbimõõt = läbimõõt;
+        PrindiInfo();
+    }
+
+    public void ArvutaPindala() {
+        double pindala = Math.PI * _raadius * _raadius;
+        Console.WriteLine($"ringi pindala:{Math.Round(pindala, 2)}");
+    }
+
+    public void ArvutaÜmbermõõt() {
+        double ümbermõõt = 2 * Math.PI * _raadius;
+        Console.WriteLine($"ringi ümbermõõt:{Math.Round(ümbermõõt, 2)}");
+    }
+}
