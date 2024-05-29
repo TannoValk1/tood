2d3ab3ec784ef2245c621c21525307809915b9af
Your Name <youremail@yourdomain.com>
2024-05-29 17:23:34 +0300

    a

--- HomeWork/Nadal14_HW/Homework_w14_1/Homework_w14_1/Homework_w14_1/Program.cs
+++ HomeWork/Nadal14_HW/Homework_w14_1/Homework_w14_1/Homework_w14_1/Program.cs
@@ -11,6 +11,13 @@ namespace Homework_w14_1
 
         static void DrawDiamond()
         {
+            int number = GetNumberOfRows();
+            DrawUpperPart(number);
+            DrawLowerPart(number);
+        }
+
+        static int GetNumberOfRows()
+        {
             int number;
             Console.Write("Sisesta ridade arv: ");
             while (!int.TryParse(Console.ReadLine(), out number) || number <= 0)
@@ -18,40 +25,48 @@ namespace Homework_w14_1
                 Console.WriteLine("See arv ei sobi!.");
                 Console.Write("Sisesta ridade arv: ");
             }
+            return number;
+        }
 
+        static void DrawUpperPart(int number)
+        {
             int spaces = number - 1;
 
             for (int row = 1; row <= number; row++)
             {
-                for (int space = 1; space <= spaces; space++)
-                {
-                    Console.Write(" ");
-                }
+                PrintSpaces(spaces);
                 spaces--;
-
-                for (int star = 1; star <= 2 * row - 1; star++)
-                {
-                    Console.Write("*");
-                }
-
+                PrintStars(2 * row - 1);
                 Console.WriteLine();
             }
+        }
 
-            spaces = 1;
+        static void DrawLowerPart(int number)
+        {
+            int spaces = 1;
+
             for (int row = number - 1; row >= 1; row--)
             {
-                for (int space = 1; space <= spaces; space++)
-                {
-                    Console.Write(" ");
-                }
+                PrintSpaces(spaces);
                 spaces++;
-
-                for (int star = 1; star <= 2 * row - 1; star++)
-                {
-                    Console.Write("*");
-                }
-
+                PrintStars(2 * row - 1);
                 Console.WriteLine();
+            }
+        }
+
+        static void PrintSpaces(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        static void PrintStars(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("*");
             }
         }
     }
--- HomeWork/Nadal14_HW/Homework_w14_3/Homework_w14_3/Homework_w14_3/Program.cs
+++ HomeWork/Nadal14_HW/Homework_w14_3/Homework_w14_3/Homework_w14_3/Program.cs
@@ -6,53 +6,50 @@ namespace Homework_w14_3
     {
         static void Main(string[] args)
         {
-            Sorting();
-        }
-
-        static void Sorting()
-        {
-            int[] numbers = GenerateRandom(10, 0, 1000);
+            var numbers = GenerateRandomArray(10, 0, 1000);
+            
             Console.WriteLine("Unsorted:");
             PrintArray(numbers);
+            
             InsertionSort(numbers);
+            
             Console.WriteLine("Sorted:");
             PrintArray(numbers);
         }
 
-        static int[] GenerateRandom(int size, int min, int max)
+        static int[] GenerateRandomArray(int size, int min, int max)
         {
-            Random rand = new Random();
-            int[] array = new int[size];
+            var random = new Random();
+            var array = new int[size];
+            
             for (int i = 0; i < size; i++)
             {
-                array[i] = rand.Next(min, max);
+                array[i] = random.Next(min, max);
             }
+            
             return array;
         }
 
         static void InsertionSort(int[] array)
         {
-            int n = array.Length;
-            for (int i = 1; i < n; i++)
+            for (int i = 1; i < array.Length; i++)
             {
-                int key = array[i];
+                var key = array[i];
                 int j = i - 1;
+                
                 while (j >= 0 && array[j] > key)
                 {
                     array[j + 1] = array[j];
-                    j = j - 1;
+                    j--;
                 }
+                
                 array[j + 1] = key;
             }
         }
 
         static void PrintArray(int[] array)
         {
-            foreach (int num in array)
-            {
-                Console.Write(num + " ");
-            }
-            Console.WriteLine();
+            Console.WriteLine(string.Join(" ", array));
         }
     }
 }
