79eb833927754287e31e5c720c553888f494d4cb
Your Name <youremail@yourdomain.com>
2024-05-29 17:10:12 +0300

    asd

---
+++ HomeWork/Nadal14_HW/Homework_w14_4/Homework_w14_4/Homework_w14_4/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Homework_w14_4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            manipulation("juhan.juurikas@company.eu");
+            createMail("juhan mart juurikas", "Juhan juurikas Company");
+        }
+
+        static public void manipulation(string mail)
+        {
+            const int maxLength = 15;
+            string sampleEmail = "juhan.juurikashruvhhre";
+
+            if (sampleEmail.Length > maxLength)
+            {
+                sampleEmail = sampleEmail.Substring(0, maxLength);
+            }
+
+            string[] sampleParts = sampleEmail.Split(new[] { ".", "@" }, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.Write("Max Length Name: ");
+            foreach (string part in sampleParts)
+            {
+                Console.Write(part + " ");
+            }
+            Console.WriteLine();
+
+            string[] nameParts = mail.Split(new[] { ".", "@company.eu" }, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.Write("Name: ");
+            foreach (string part in nameParts)
+            {
+                Console.Write(part.ToUpper() + " ");
+            }
+            Console.WriteLine();
+
+            string[] domainParts = mail.Split(new[] { "juhan.juurikas@", ".eu" }, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.Write("Domain: ");
+            foreach (string part in domainParts)
+            {
+                Console.Write(part + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static public void createMail(string name, string organization)
+        {
+            Console.Write("Name: ");
+            Console.WriteLine(name);
+
+            Console.Write("Mail: ");
+            string formattedName = name.Replace(' ', '.').Replace(',', '.');
+            Console.WriteLine(formattedName + "@Company.eu");
+
+            Console.Write("Name: ");
+            Console.WriteLine(organization);
+
+            Console.Write("Mail: ");
+            string formattedOrganization = organization.Replace(" ", "").Replace('.', ' ').Replace(':', '@');
+            Console.WriteLine(formattedOrganization + ".eu");
+        }
+    }
+}
